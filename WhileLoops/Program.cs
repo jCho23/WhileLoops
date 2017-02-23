5447d8a311ae9a6859b355ddd87955edad16df06
JuneCho23 <junecho.ucb@gmail.com>
2017-02-23 10:26:38 -0800

    While Loops

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace WhileLoops
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+
+		{
+			for (int i = 1; i <= 5; i++)
+			{
+				Console.WriteLine(i);
+			}
+
+
+
+			//while (x < 5)
+			//{
+			//	Console.WriteLine(x++);
+			//}
+			//Console.WriteLine("While Loop Done");
+			//Console.WriteLine(x);
+
+			////2
+			////3
+			////4
+			////5
+		}
+
+	}
+}
